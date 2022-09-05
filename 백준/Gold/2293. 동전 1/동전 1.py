435fa59a5fbb80e7f71941165a9aa5d82198676e
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-09-05 20:52:40 +0900

    [Gold V] Title: 동전 1, Time: 136 ms, Memory: 116172 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -38,6 +38,11 @@
 # dp로 풀어야함
 # 왜 dp? 해당 차수의 경우의 수를 구할 때 앞에서 구했던 경우의 수를 이용하기 때문에!
 # https://velog.io/@ready2start/Python-%EB%B0%B1%EC%A4%80-2293-%EB%8F%99%EC%A0%84-1
+#    1   2   3   4   5   6   7   8   9   10
+# 5  0   0   0   0   1   0   0   0   0   1
+# 2  0   1   0   1   0   1   1   1   1   1
+# 1  1   1   2   2   3   4   5   6   7   8
+
 if __name__ == "__main__":
     n, k = map(int, input().split())
     coins = []
@@ -45,7 +50,7 @@ if __name__ == "__main__":
         coins.append(int(input()))
     
     dp = [0] * (k+1)
-    dp[0] = 1
+    dp[0] = 1 # 동전 1개 쓰는 경우를 위해!
 
     for coin in coins:
         for i in range(1, k+1):
