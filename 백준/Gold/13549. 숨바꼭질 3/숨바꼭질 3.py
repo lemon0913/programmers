3350b7d56c6a474962c8b30f516df01bb396278d
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-09-09 01:07:56 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 180 ms, Memory: 117152 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -11,12 +11,13 @@ while q:
     if s == k:
         print(visited[s])
         break
-    if 0 <= s-1 < 100001 and visited[s-1] == -1:
-        visited[s-1] = visited[s] + 1
-        q.append(s-1)
-    if 0 < s*2 < 100001 and visited[s*2] == -1:
-        visited[s*2] = visited[s]
-        q.appendleft(s*2)  # 2*s 가 다른 연산보다 더 높은 우선순위를 가지기 위함
-    if 0 <= s+1 < 100001 and visited[s+1] == -1:
-        visited[s+1] = visited[s] + 1
-        q.append(s+1)
+    else:
+        if 0 <= s-1 < 100001 and visited[s-1] == -1:
+            visited[s-1] = visited[s] + 1
+            q.append(s-1)
+        if 0 < s*2 < 100001 and visited[s*2] == -1:
+            visited[s*2] = visited[s]
+            q.appendleft(s*2)  # 2*s 가 다른 연산보다 더 높은 우선순위를 가지기 위함
+        if 0 <= s+1 < 100001 and visited[s+1] == -1:
+            visited[s+1] = visited[s] + 1
+            q.append(s+1)
