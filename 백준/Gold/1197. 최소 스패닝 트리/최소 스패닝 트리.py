f4b584d9d3038b74b5d4a2484913ccc38d841e4a
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-09-24 01:02:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 524 ms, Memory: 133084 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,40 +1,35 @@
-# 책이랑 똑같이 풀면 된다
 if __name__ == "__main__":
-
-    def find(parent, x):
+    
+    def find_parent(parent,x):
         if parent[x] != x:
-            parent[x] = find(parent, parent[x])
+            parent[x] = find_parent(parent, parent[x])
         return parent[x]
     
-    def union(parent, a, b):
-        a = find(parent, a)
-        b = find(parent, b)
+    def union_parent(parent,a,b):
+        a = find_parent(parent,a)
+        b = find_parent(parent,b)
+
         if a < b:
             parent[b] = a
         else:
             parent[a] = b
-
-
+    
     V, E = map(int, input().split())
-    parent = [0] * (V+1)
+    parent = [0]*(V+1)
     for i in range(1, V+1):
         parent[i] = i
     
     edges = []
     result = 0
-
-    for _ in range(E):
-        a, b, c = map(int, input().split())
-        edges.append((c,a,b))
-    
+    for i in range(E):
+        A,B,C = map(int, input().split())
+        edges.append((C,A,B))
     edges.sort()
 
     for e in edges:
-        c, a, b = e
-        a = find(parent, a)
-        b = find(parent, b)
-        if a != b:
-            result += c
-            union(parent, a, b)
-        
+        cost, a, b = e
+        if find_parent(parent,a) != find_parent(parent,b):
+            union_parent(parent,a,b)
+            result += cost 
+    
     print(result)
