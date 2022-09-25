51ad971653afda3631de6956c94d8b3bddfd82a3
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-09-25 15:44:47 +0900

    [Gold III] Title: 줄 세우기, Time: 284 ms, Memory: 123568 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,30 +1,32 @@
-# 책이랑 똑같이 풀면 됨
 from collections import deque
+
 if __name__ == "__main__":
     N, M = map(int, input().split())
-
+    indegree = [0]*(N+1)
     graph = [[] for _ in range(N+1)]
-    indegree = [0] * (N+1)
-
     for _ in range(M):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        indegree[b] += 1
+        A, B = map(int, input().split())
+        graph[A].append(B)
+        indegree[B] += 1
     
-    result = []
-    q = deque()
+    def topology():
+        q = deque()
+        result = []
 
-    for i in range(1, N+1):
-        if indegree[i] == 0:
-            q.append(i)
-    
-    while q:
-        now = q.popleft()
-        result.append(now)
-        for i in graph[now]:
-            indegree[i] -= 1
+        for i in range(1, N+1):
             if indegree[i] == 0:
                 q.append(i)
+        
+        while q:
+            now = q.popleft()
+            result.append(now)
+
+            for x in graph[now]:
+                indegree[x] -= 1
+                if indegree[x] == 0:
+                    q.append(x)
+        
+        for i in result:
+            print(i, end = ' ')
     
-    for i in result:
-        print(i, end=' ')
+    topology()
