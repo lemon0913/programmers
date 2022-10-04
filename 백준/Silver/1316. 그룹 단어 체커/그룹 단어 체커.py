41d43f3f40b014375fb032f6a250950c144a0031
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-10-04 22:38:03 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    N = int(input())
+    count = N
+
+    for _ in range(N):
+        word = input()
+        for i in range(0, len(word)-1):
+            if word[i] == word[i+1]:
+                pass
+            elif word[i] in word[i+1:]:
+                count -= 1
+                break
+
+    print(count)
