03f76502501152c9376dce1dce3060f25fb83a68
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-10-13 16:37:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 608 ms, Memory: 128700 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,21 @@
+
+N, S = map(int, input().split())
+
+nums = list(map(int, input().split()))
+
+result = 0
+def dfs(s, x):
+    global result
+    
+    if len(s) == N:
+        return
+    
+    for i in range(x, N):
+        s.append(nums[i])
+        if sum(s) == S:
+            result += 1
+        dfs(s, i+1)
+        s.pop()
+
+dfs([], 0)
+print(result)
