b3ba5b74d71f10abb14bd04369178451cf151fa5
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-10-14 15:11:22 +0900

    [Gold V] Title: 토마토, Time: 412 ms, Memory: 159080 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,46 +3,94 @@ from collections import deque
 
 M, N = map(int, input().split())
 graph = []
-for i in range(N):
+for _ in range(N):
     graph.append(list(map(int, input().split())))
 
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-q = deque([])
+q = deque()
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 1:
             q.append((i,j))
 
-def bfs():
-    
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = graph[x][y] + 1
-                    q.append((nx,ny))
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y]+1
+                q.append((nx,ny))
 
-bfs()
 
 flag = True
+m = -1
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 0:
-            print(-1)
             flag = False
             break
+        elif graph[i][j] > m:
+            m = graph[i][j]
     if not flag:
         break
 
-m = 0
-if flag:
-    for i in range(N):
-        m = max(max(graph[i]), m)
+if not flag:
+    print(-1)
+else:
     print(m-1)
+
+
+
+# from collections import deque
+
+
+# M, N = map(int, input().split())
+# graph = []
+# for _ in range(N):
+#     graph.append(list(map(int, input().split())))
+
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# def bfs(graph, a, b):
+#     q = deque([(a,b)])
+
+#     while q:
+#         x,y = q.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+
+#             if 0 <= nx < N and 0 <= ny < M:
+#                 if graph[nx][ny] == 0 or graph[nx][ny] > graph[x][y] + 1:
+#                     graph[nx][ny] = graph[x][y] + 1
+#                     q.append((nx,ny))
+
+# for i in range(N):
+#     for j in range(M):
+#         if graph[i][j] == 1:
+#             bfs(graph,i,j)
+
+# flag = True
+# m = -1
+# for i in range(N):
+#     for j in range(M):
+#         if graph[i][j] == 0:
+#             flag = False
+#             break
+#         elif graph[i][j] > m:
+#             m = graph[i][j]
+#     if not flag:
+#         break
+
+# if not flag:
+#     print(-1)
+# else:
+#     print(m-1)
