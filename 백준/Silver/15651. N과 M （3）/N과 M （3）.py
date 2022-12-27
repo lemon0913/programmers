96afbbe6428031db0359e38fe05015ee27cfeff4
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2022-12-28 01:03:01 +0900

    [Silver III] Title: N과 M (3), Time: 636 ms, Memory: 124264 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,17 +1,13 @@
-
 N, M = map(int, input().split())
-
-nums = list(range(1, N+1))
 
 def dfs(s):
     if len(s) == M:
-        print(' '.join(list(map(str,s))))
+        print(' '.join(map(str,s)))
         return
     
-
-    for i in range(1, N+1):
-        s.append(i)
-        dfs(s)
-        s.pop()
+    for i in range(1,N+1):
+            s.append(i)
+            dfs(s)
+            s.pop()
 
 dfs([])
