b2fe0b51bfe1a27d553c471caf6c9a43c9e7f193
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2023-01-16 15:21:29 +0900

    [Gold V] Title: 무기 공학, Time: 496 ms, Memory: 122376 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.py
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -1,32 +1,32 @@
 
-N, M = map(int, input().split())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
+n, m = map(int, input().split())
+tree = []
+for _ in range(n):
+    tree.append(list(map(int, input().split())))
 
+result = 0
+visited = [[False]*m for _ in range(n)]
+shape = [[0,-1,1,0],[-1,0,0,-1],[-1,0,0,1],[0,1,1,0]]
 
-result = []
-visited = [[False]*M for _ in range(N)]
-shape = {0:[0,-1,1,0], 1:[-1,0,0,-1], 2:[-1,0,0,1], 3:[0,1,1,0]}
-def dfs(i,j,s):
-    # 종료 조건을 못 잡음...
-    if j == M:
-        i += 1
-        j = 0
-    if i == N:
-        result.append(s)
+def dfs(a,b,visited,r):
+    global result
+    if b == m:
+        a += 1
+        b = 0
+    if a == n:
+        result = max(result,r)
         return
     
-    if not visited[i][j]:
+    if not visited[a][b]:
         for k in range(4):
-            a,b,c,d = shape[k]
-            x,y,xx,yy = i+a, j+b, i+c, j+d
-            if 0 <= x < N and 0 <= y < M and 0 <= xx < N and 0 <= yy < M:
-                if not visited[x][y] and not visited[xx][yy]:
-                    visited[i][j] = visited[x][y] = visited[xx][yy] = True
-                    dfs(i,j+1,s+graph[i][j]*2+graph[x][y]+graph[xx][yy])
-                    visited[i][j] = visited[x][y] = visited[xx][yy] = False
-    dfs(i,j+1,s)
+            x,y,xx,yy = a+shape[k][0], b+shape[k][1], a+shape[k][2], b+shape[k][3]
+            if 0 <= x < n and 0 <= y < m and 0 <= xx < n and 0 <= yy < m and not visited[x][y] and not visited[xx][yy]:
+                visited[a][b] = visited[x][y] = visited[xx][yy] = True
+                dfs(a,b+1,visited,r+tree[a][b]*2+tree[x][y]+tree[xx][yy])
+                visited[a][b] = visited[x][y] = visited[xx][yy] = False
+    
+    dfs(a,b+1,visited,r)
 
-dfs(0,0,0)
-print(max(result))
+dfs(0,0,visited,0)
+
+print(result)
