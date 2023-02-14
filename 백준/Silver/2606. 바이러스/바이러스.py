033f87f868344cbf9af122a1312dcb9f000d9a80
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2023-02-15 06:58:01 +0900

    [Silver III] Title: 바이러스, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,25 @@
-import sys
-if __name__ == "__main__":
-    N = int(input())
-    c = int(input())
-    connect = [[]*N for _ in range(N+1)]
-    for i in range(c):
-        a, b = map(int, input().split())
-        connect[a].append(b)
-        connect[b].append(a)
-    
-    cnt = 0
-    visited = [False] * (N+1)
+from collections import deque
 
-    def dfs(v):
-        global cnt
-        # 현재 노드를 방문 처리
-        visited[v] = True
-        # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-        for i in connect[v]:
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+visited = [False]*(n+1)
+def bfs(start):
+    visited[start] = True
+    q = deque([start])
+
+    while q:
+        x = q.popleft()
+        for i in graph[x]:
             if not visited[i]:
-                dfs(i)
-                cnt += 1
+                visited[i] = True
+                q.append(i)
 
-    dfs(1)
-    print(cnt)
+bfs(1)
+print(visited.count(True)-1)
