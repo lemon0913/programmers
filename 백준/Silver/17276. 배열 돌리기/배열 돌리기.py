c74dfeb2d647c9512cafe0f05ed8fb3dc1fbedf9
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2023-02-27 22:58:37 +0900

    [Silver II] Title: 배열 돌리기, Time: 512 ms, Memory: 126580 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -1,83 +1,76 @@
 
+def right(graph,c,n):
+    for _ in range(c):
+        nn = n//2
 
-def right(m):
-    global n
-    tmp = [0]*n
-
-    t = n//2
-    for i in range(n):
-        tmp[i] = m[t][i]
-    
-    # 1. 부 대각선을 가운데 행으로
-    for i in range(n):
-        m[t][i] = m[n-1-i][i]
-    
-    # 2. 가운데 열을 부 대각선으로
-    for i in range(n):
-        m[i][n-1-i] = m[i][t]
-    
-    # 3. 주 대각선을 가운데 열로
-    for i in range(n):
-        m[i][t] = m[i][i]
-    
-    # 4. 가운데 행을 주 대각선으로
-    for i in range(n):
-        m[i][i] = tmp[i]
+        # 1)주대각선의 값 임시 저장하기
+        tmp = [0]*(n)
+        for i in range(n):
+            tmp[i] = graph[i][i]
+        
+        # 2)가운데 행의 값을 주대각선에 집어넣기
+        for i in range(n):
+            graph[i][i] = graph[nn][i]
+        
+        # 3)부대각선의 값을 가운데 행에 집어넣기
+        for i in range(n):
+            graph[nn][i] = graph[n-1-i][i]
+        
+        # 4)가운데 열의 값을 부대각선에 집어넣기
+        for i in range(n): 
+            graph[n-1-i][i] = graph[n-1-i][nn]
+        
+        # 5)임시 저장한 값을 가운데 열에 집어 넣기
+        for i in range(n):
+            graph[i][nn] = tmp[i]
     
 
-def left(m):
-    global n
-    tmp = [0]*n
 
-    t = n//2
-    for i in range(n):
-        tmp[i] = m[t][i]
-    
-    # 1. 주 대각선을 가운데 행으로
-    for i in range(n):
-        m[t][i] = m[i][i]
-    
-    # 2. 가운데 열을 주 대각선으로
-    for i in range(n):
-        m[i][i] = m[i][t]
-    
-    # 3. 부 대각선을 가운데 열로
-    for i in range(n):
-        m[i][t] = m[i][n-1-i]
+def left(graph,c,n):
+    for _ in range(c):
+        nn = n//2
 
-    # 4. 가운데 행을 부 대각선으로
-    for i in range(n):
-        m[n-1-i][i] = tmp[i]
+        # 1) 주대각선의 값을 임시 저장하기
+        tmp = [0]*(n)
+        for i in range(n):
+            tmp[i] = graph[i][i]
+        
+        # 2) 가운데 열의 값을 주대각선에 집어넣기
+        for i in range(n):
+            graph[i][i] = graph[i][nn]
+        
+        # 3) 부대각선의 값을 가운데 열에 집어 넣기
+        for i in range(n):
+            graph[i][nn] = graph[i][n-1-i]
+        
+        # 4) 가운데 행의 값을 부대각선에 집어넣기
+        for i in range(n):
+            graph[n-1-i][i] = graph[nn][i]
+        
+        # 5) 임지 저장한 값을 가운데 행에 집어넣기
+        for i in range(n):
+            graph[nn][i] = tmp[i]
     
-    
-    
-T = int(input())   
 
-for _ in range(T):
-    n, d = map(int, input().split())
+
+
+t = int(input())
+for _ in range(t):
     
-    matrix = []
+    n,d = map(int, input().split())
+    graph = []
     for _ in range(n):
-        matrix.append(list(map(int, input().split())))
+        graph.append(list(map(int, input().split())))
     
-    
-    c = d // 45
 
-    if c < 0:
-        c = c*(-1)
-        for _ in range(c):
-            left(matrix)
-        
-        for i in range(n):
-            for j in range(n):
-                print(matrix[i][j], end=' ')
-            print()
-        
-    else:
-        for _ in range(c):
-            right(matrix)
-        
-        for i in range(n):
-            for j in range(n):
-                print(matrix[i][j], end=' ')
-            print()
+    if d > 0:
+        c = d//45
+        right(graph,c,n)
+    
+    elif d < 0:
+        c = d*(-1)//45
+        left(graph,c,n)
+    
+
+    for i in range(n):
+        print(' '.join(map(str,graph[i])))
