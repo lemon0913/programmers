31e94cd3cdb9ff2541ca7bac6b33c86641f54831
lemon0913 <86991963+lemon0913@users.noreply.github.com>
2023-08-15 17:30:54 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuffer;
+
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuffer sb = new StringBuffer(my_string);
+        String reverse = sb.reverse().toString();
+        
+        return reverse;
+    }
+}
